c01e654cc9f51f5873e661d4724007caa26f0150
Yujia Xie <yxieGT@hotmail.com>
2016-07-21 22:50:57 +0200

    test

--- popup.js
+++ popup.js
@@ -6,32 +6,33 @@ document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('dyslexia');
     checkPageButton.addEventListener('click', function() {
 
-    }, false);
+    });
 
     //enlarge
     var enlargeButton = document.getElementById('enlarge');
     enlargeButton.addEventListener('click', function() {
         // var x = document.getElementByTagName('P');
+        alert("here")
         var x = $('p');
         var i;
         for (i = 0; i < x.length; i++) {
             x[i].style.backgroundColor = "red";
         }
         console.log(11);
-        document.getElementByTagName('P').innerHTML = x;
+        document.getElementsByTagName('P').innerHTML = x;
         console.log(6+6);
-    }, false);
+    });
 
     //greyscale
     var greyscaleButton = document.getElementById('greyscale');
     greyscaleButton.addEventListener('click', function() {
 
-    }, false);
+    });
 
     //paraphrase
     var paraphraseButton = document.getElementById('paraphrase');
     paraphraseButton.addEventListener('click', function() {
 
-    }, false);
+    });
 
-}, false);
+});
