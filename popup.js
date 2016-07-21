6a41641e8fdffe3bf79df2a609512594f9963d2e
Yujia Xie <yxieGT@hotmail.com>
2016-07-21 20:26:09 +0200

    initiate buttons

---
+++ popup.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    //dyslexia
+    var checkPageButton = document.getElementById('dyslexia');
+    checkPageButton.addEventListener('click', function() {
+
+    });
+
+    //enlarge
+    var enlargeButton = document.getElementById('enlarge');
+    enlargeButton.addEventListener('click', function() {
+
+    });
+
+    //greyscale
+    var greyscaleButton = document.getElementById('greyscale');
+    greyscaleButton.addEventListener('click', function() {
+
+    });
+
+    //paraphrase
+    var paraphraseButton = document.getElementById('paraphrase');
+    paraphraseButton.addEventListener('click', function() {
+
+    });
+
+});
